ed35e8c498b3b1fd9283a7014fab7454e5888004
Raymond Akalonu <raymondosy1@gmail.com>
2019-10-06 18:52:10 +0100

    Updates

---
+++ precache-manifest.537f5249470e6e4b0239e205d7124544.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07abca18c4762a75d8fc6c9a27f0f589",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66713bd33079b480b23f",
+    "url": "/static/css/main.52a744cd.chunk.css"
+  },
+  {
+    "revision": "f03332af716a07ee684e",
+    "url": "/static/js/2.0aa5d56e.chunk.js"
+  },
+  {
+    "revision": "66713bd33079b480b23f",
+    "url": "/static/js/main.80299dca.chunk.js"
+  },
+  {
+    "revision": "909e8db6bc62c7804590",
+    "url": "/static/js/runtime-main.d97d85ad.js"
+  }
+]);
